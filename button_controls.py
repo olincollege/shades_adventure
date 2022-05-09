edcae138966d0461e23de739c216e2599180be14
Mateo Macias <mmacias@olin.edu>
2022-05-09 06:03:21 -0400

    Finalize docstrings for button_controls.py

--- button_controls.py
+++ button_controls.py
@@ -3,15 +3,28 @@ import pygame
 
 class Button():
     """
+    Button class for tracking where the mouse is as well as where it clicks on
+    the display.
     """
 
     def __init__(self, surface, rect):
+        """
+        Constructs all the necessary attributes for the Button class.
+
+        Attributes:
+            surface: The game window.
+            rect: An instance of GameView that uses the draw_button method to
+            get the dimensions of a button.
+            clicked: A boolean that determines if the button is clicked.
+        """
+        self.surface = surface
         self.rect = rect
         self.clicked = False
-        self.surface = surface
 
     def get_button(self):
         """
+        Determine the mouse position and determine if it has clicked a certain
+        area.
         """
         action = False
         mouse_pos = pygame.mouse.get_pos()
